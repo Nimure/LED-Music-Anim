0d8b3e074bbc4bdad636526cd90663da455aee3a
Nimure <kechara19@gmail.com>
2022-10-06 22:12:16 -0400

    initial blink

--- 1-LightAnim/BlinkTest.py
+++ 1-LightAnim/BlinkTest.py
@@ -1,8 +1,16 @@
+import time
+import board
 import neopixel
 import random
 
-print("hello blinky!")
+#print("hello blinky!")
 
 pixels = neopixel.NeoPixel(board.D21, 300)
 
-pixels[0] = (255, 0, 0)
+# pixels[0] = (255, 0, 0)
+
+ while True:
+    pixels[0] = (255, 0, 0)
+    time.sleep(0.5)
+    pixels[0] = (0, 0, 0)
+    time.sleep(0.5)

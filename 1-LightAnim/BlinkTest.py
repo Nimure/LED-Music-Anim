8e6cf104b43bc2a608dcb5a803c27b3f7f0a6747
Nimure <kechara19@gmail.com>
2022-10-06 22:16:34 -0400

    blink colors

--- 1-LightAnim/BlinkTest.py
+++ 1-LightAnim/BlinkTest.py
@@ -9,8 +9,24 @@ pixels = neopixel.NeoPixel(board.D21, 300)
 
 # pixels[0] = (255, 0, 0)
 
+# while True:
+#    pixels[0] = (255, 0, 0)
+#    time.sleep(0.5)
+#    pixels[0] = (0, 0, 0)
+#    time.sleep(0.5)
+
  while True:
-    pixels[0] = (255, 0, 0)
-    time.sleep(0.5)
-    pixels[0] = (0, 0, 0)
-    time.sleep(0.5)
+     pixels.fill((255, 0, 0))
+     time.sleep(0.5)
+     pixels.fill((255, 127, 0))
+     time.sleep(0.5)
+     pixels.fill((255, 255, 0))
+     time.sleep(0.5)
+     pixels.fill((0, 255, 0))
+     time.sleep(0.5)
+     pixels.fill((0, 0, 225))
+     time.sleep(0.5)
+     pixels.fill((75, 0, 130))
+     time.sleep(0.5)
+     pixels.fill((148, 0, 211))
+     time.sleep(0.5)
